7baf92290f5812ce1d2bd6f6efd1553dd56d6181
Guido Falcucci <guido.falcucci@hotmail.com>
2014-12-08 11:17:28 -0200

    Wrap google maps api in class

--- app/helpers/api/localities_helper.rb
+++ app/helpers/api/localities_helper.rb
@@ -1,10 +1,20 @@
 module Api::LocalitiesHelper
-  GOOGLE_MAPS = "http://maps.googleapis.com/maps/api/geocode/json?latlng=COORDINATES&sensor=true_or_false"
+  class GoogleMapsFinder
+    GOOGLE_MAPS = "http://maps.googleapis.com/maps/api/geocode/json?latlng=COORDINATES&sensor=true_or_false"
+    def find_locality(latitude,longitude)
+      coordinates = latitude + "," + longitude
+      uri = URI.parse(GOOGLE_MAPS.gsub("COORDINATES",coordinates))
+      response = Net::HTTP.get(uri)
+      json = JSON.parse(response)
+      json['results'][0]['address_components'][2]['long_name']
+    end
+  end
+
+  def finder
+    @finder ||= GoogleMapsFinder.new
+  end
+
   def find_locality(latitude,longitude)
-    coordinates = latitude + "," + longitude
-    uri = URI.parse(GOOGLE_MAPS.gsub("COORDINATES",coordinates))
-    response = Net::HTTP.get(uri)
-    json = JSON.parse(response)
-    json['results'][0]['address_components'][2]['long_name']
+    finder.find_locality(latitude,longitude)
   end
 end
