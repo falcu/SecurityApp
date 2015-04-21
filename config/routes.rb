bd4d26f013043161ecdcdcd3627f19ccf97663ef
falcu <guido.falcucci@hotmail.com>
2015-04-20 21:16:42 -0300

    Fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  root 'welcome/index'
-
   namespace :api do
 
     post "users/create", to: "users#create", as: "create"
@@ -29,7 +27,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
